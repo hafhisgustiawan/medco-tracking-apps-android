2e7aa03db9e4d44630c95f6e43fb908012a1e3d8
Hafhis Gustiawan <hafhisgustiawan@gmail.com>
2024-01-13 16:38:29 +0700

    initial commit

---
+++ app/src/main/java/com/medco/trackingapp/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.medco.trackingapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.medco.trackingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
