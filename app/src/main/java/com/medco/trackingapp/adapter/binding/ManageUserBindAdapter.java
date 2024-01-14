3608a318b08581495a2dec89f73eec0c93f5bb29
Hafhis Gustiawan <hafhisgustiawan@gmail.com>
2024-01-15 00:46:30 +0700

    adding features manage user

--- app/src/main/java/com/medco/trackingapp/adapter/binding/ManageUserBindAdapter.java
+++ app/src/main/java/com/medco/trackingapp/adapter/binding/ManageUserBindAdapter.java
@@ -2,7 +2,10 @@ package com.medco.trackingapp.adapter.binding;
 
 import androidx.databinding.BindingAdapter;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.medco.trackingapp.helper.ValidationEmail;
+import com.medco.trackingapp.helper.ValidationPhone;
 import com.medco.trackingapp.model.viewmodel.ManageUserItem;
 
 import java.util.Objects;
@@ -13,15 +16,51 @@ public class ManageUserBindAdapter {
 		til.setError(null);
 		if (item == null || item.getName() == null) return;
 		if (Objects.equals(item.getName(), "") || item.getName().length() > 5) return;
-
 		til.setError("Minimal 5 karakter");
 	}
 
 	@BindingAdapter("validateTilManageUserEmail")
 	public static void validateTilManageUserEmail(TextInputLayout til, ManageUserItem item) {
 		til.setError(null);
-		if (item == null || item.getName() == null) return;
-
+		if (item == null || item.getEmail() == null) return;
+		if (new ValidationEmail().isValidEmail(item.getEmail())) return;
 		til.setError("Email tidak valid");
+	}
+
+	@BindingAdapter("validateTilManageUserPhone")
+	public static void validateTilManageUserPhone(TextInputLayout til, ManageUserItem item) {
+		til.setError(null);
+		if (item == null || item.getPhone() == null) return;
+		if (new ValidationPhone().isValidPhone(item.getPhone())) return;
+		til.setError("Telepon tidak valid, gunakan awalan +62 atau 08");
+	}
+
+	@BindingAdapter("validateManageUserBtn")
+	public static void validateManageUserBtn(MaterialButton btn, ManageUserItem item) {
+		btn.setEnabled(false);
+
+		// 0) NULL CHECK
+		if (item == null) return;
+
+		if (item.getName() == null || item.getEmail() == null || item.getPhone() == null) {
+			return;
+		}
+
+		if (Objects.equals(item.getName(), "") || Objects.equals(item.getEmail(), "")
+			|| Objects.equals(item.getPhone(), "")) {
+			return;
+		}
+
+		// 1) NAME CHECK
+		if (item.getName().length() < 5) return;
+
+		// 2) EMAIL CHECK
+		if (!(new ValidationEmail().isValidEmail(item.getEmail()))) return;
+
+		// 3) PHONE CHECK
+		if (!(new ValidationPhone().isValidPhone(item.getPhone()))) return;
+
+		// 4) ALL BYPASS AND ENABLE BUTTON
+		btn.setEnabled(true);
 	}
 }
--- app/src/main/java/com/medco/trackingapp/fragment/ManageUserFragment.java
+++ app/src/main/java/com/medco/trackingapp/fragment/ManageUserFragment.java
@@ -1,6 +1,7 @@
 package com.medco.trackingapp.fragment;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,30 +13,41 @@ import android.view.WindowManager;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.medco.trackingapp.R;
 import com.medco.trackingapp.databinding.FragmentManageUserBinding;
+import com.medco.trackingapp.helper.EtWatcher;
 import com.medco.trackingapp.helper.SnackbarHelper;
 import com.medco.trackingapp.model.UserItem;
+import com.medco.trackingapp.model.viewmodel.ManageUserItem;
 import com.medco.trackingapp.model.viewmodel.ManageUserViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class ManageUserFragment extends BottomSheetDialogFragment {
 
 	public static final String TAG = "ManageUserFragment";
+	private final DocumentSnapshot mUserSnapshot;
 	//callback
 	public ListenerClose listenerClose;
 	private Context mContext;
 	private SnackbarHelper snackbarHelper;
 	private FragmentManageUserBinding binding;
-	private DocumentSnapshot userSnapshot;
+	private CollectionReference userColl;
 	private ManageUserViewModel mViewModel;
 
 	public ManageUserFragment(DocumentSnapshot userSnapshot) {
 		// Required empty public constructor
-		this.userSnapshot = userSnapshot;
+		this.mUserSnapshot = userSnapshot;
 	}
 
 	public void ListenerApiClose(ListenerClose listener) {
@@ -55,6 +67,9 @@ public class ManageUserFragment extends BottomSheetDialogFragment {
 		snackbarHelper = new SnackbarHelper(Objects.requireNonNull(requireDialog()
 			.getWindow()).getDecorView(), binding.spacer);
 
+		FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+		userColl = firebaseFirestore.collection(getString(R.string.collection_user));
+
 		mViewModel = new ViewModelProvider(this).get(ManageUserViewModel.class);
 
 		initViews();
@@ -66,8 +81,8 @@ public class ManageUserFragment extends BottomSheetDialogFragment {
 		mViewModel.getManageUserState().observe(this, manageUserItem -> binding
 			.setManageUserItem(manageUserItem));
 
-		if (userSnapshot == null) return;
-		UserItem userItem = userSnapshot.toObject(UserItem.class);
+		if (mUserSnapshot == null) return;
+		UserItem userItem = mUserSnapshot.toObject(UserItem.class);
 		binding.setUserItem(userItem);
 	}
 
@@ -75,6 +90,103 @@ public class ManageUserFragment extends BottomSheetDialogFragment {
 		binding.tvClose.setOnClickListener(view -> {
 			if (listenerClose == null) return;
 			listenerClose.listenerClose(0);
+		});
+
+		binding.etName.addTextChangedListener(new EtWatcher(str -> {
+			ManageUserItem item = mViewModel.getManageUserState().getValue();
+			if (item == null) return;
+			item.setName(str);
+			mViewModel.setManageUserState(item);
+		}));
+
+		binding.etEmail.addTextChangedListener(new EtWatcher(str -> {
+			ManageUserItem item = mViewModel.getManageUserState().getValue();
+			if (item == null) return;
+			item.setEmail(str.trim());
+			mViewModel.setManageUserState(item);
+		}));
+
+		binding.etPhone.addTextChangedListener(new EtWatcher(str -> {
+			ManageUserItem item = mViewModel.getManageUserState().getValue();
+			if (item == null) return;
+			item.setPhone(str.trim());
+			mViewModel.setManageUserState(item);
+		}));
+
+		binding.btnSave.setOnClickListener(view -> {
+			String name = binding.etName.getText() != null ? binding.etName.getText()
+				.toString().trim() : null;
+			String email = binding.etEmail.getText() != null ? binding.etEmail.getText()
+				.toString().trim() : null;
+			String phone = binding.etPhone.getText() != null ? binding.etPhone.getText()
+				.toString().trim() : null;
+
+			new AlertDialog.Builder(mContext)
+				.setCancelable(true)
+				.setMessage("Anda yakin data sudah benar?")
+				.setNegativeButton("Tidak", null)
+				.setPositiveButton("Ya", (dialogInterface, i) -> {
+					if (mUserSnapshot == null) {
+						addUser(name, email, phone);
+					} else {
+						updateUser(name, email, phone);
+					}
+				}).create().show();
+		});
+	}
+
+	private void addUser(String name, String email, String phone) {
+		UserItem userItem = new UserItem();
+		userItem.setName(name);
+		userItem.setEmail(email);
+		userItem.setPhone(phone);
+		userItem.setRole("user");
+		userItem.setTimeRegister(Timestamp.now());
+		userItem.setUidDevice(null);
+		userItem.setPhoto(null);
+
+		showProgress();
+		userColl.whereEqualTo("email", email).get().addOnCompleteListener(task -> {
+			dismissProgress();
+			if (!task.isSuccessful()) {
+				showError(task.getException());
+				return;
+			}
+
+			if (!task.getResult().isEmpty()) {
+				snackbarHelper.show("Email tidak bisa duplikat", Snackbar.LENGTH_INDEFINITE);
+				return;
+			}
+
+			showProgress();
+			userColl.add(userItem).addOnCompleteListener(t -> {
+				dismissProgress();
+				if (!t.isSuccessful()) {
+					showError(t.getException());
+					return;
+				}
+				if (listenerClose == null) return;
+				listenerClose.listenerClose(1);
+			});
+		});
+	}
+
+	private void updateUser(String name, String email, String phone) {
+		List<Task<Void>> tasks = new ArrayList<>();
+		tasks.add(mUserSnapshot.getReference().update("name", name));
+		tasks.add(mUserSnapshot.getReference().update("email", email));
+		tasks.add(mUserSnapshot.getReference().update("phone", phone));
+
+		showProgress();
+		Tasks.whenAllComplete(tasks).addOnCompleteListener(task -> {
+			dismissProgress();
+			if (!task.isSuccessful()) {
+				showError(task.getException());
+				return;
+			}
+
+			if (listenerClose == null) return;
+			listenerClose.listenerClose(1);
 		});
 	}
 
